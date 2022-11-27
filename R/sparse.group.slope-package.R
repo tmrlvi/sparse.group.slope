c5e927462894315a59454f5cbc30edb1b9974916
Tomer Levy <tmrlvi@gmail.com>
2022-11-27 07:04:32 +0200

    fixing compilaation issues

---
+++ R/sparse.group.slope-package.R
@@ -0,0 +1,6 @@
+  ## usethis namespace: start
+  #' @useDynLib sparse.group.slope, .registration = TRUE
+  #' @exportPattern "^[^\\.]"
+  #' @importFrom Rcpp sourceCpp
+  ## usethis namespace: end
+  NULL
