8069b0f68d942a52b99f7d189c63b4aa85394208
Tomer Levy <tmrlvi@gmail.com>
2022-11-27 04:13:41 +0200

    proximal test works

--- src/test-prox.cpp
+++ src/test-prox.cpp
@@ -1,4 +1,5 @@
 #include <testthat.h>
+#include <cmath>
 #include <iostream>
 #include <RcppArmadillo.h>
 #include "penalties/sparse_group_slope.hpp"
@@ -11,36 +12,29 @@ context("Proximal Calculations") {
   // unit test, and use 'expect_true()' and 'expect_false()'
   // to test the desired conditions.
   test_that("prox calculation correct on examples") {
-    std::cout << "d" << std::endl;;
-    const double lambda_init[] = {1, 0, 0};
-    arma::vec lambda(&lambda_init[0], 3);
-    //arma::vec lambda(3, arma::fill::ones);
+    const double expected_prox_arr[] = {
+      7.97740041261, 3.65160027507, 0, 
+      6.38192033009, 2.92128022005, 0, 
+      4.78644024756, 2.19096016505, 0, 
+      3.19096016505, 1.46064011003, 0, 
+      1.59548008252, 0.73032005501, 0
+    };
+    arma::mat expected_prox(&expected_prox_arr[0], 3, 5);
 
-    const double kappa_init[] = {1, 0, 0, 0, 0};
+    const double lambda_init[] = {3, 2, 1};
+    arma::vec lambda(&lambda_init[0], 3);
+
+    const double kappa_init[] = {5, 4, 3, 2, 1};
     arma::vec kappa(&kappa_init[0], 5);
-    //arma::vec kappa(5, arma::fill::ones);
-    std::cout << "d" << std::endl;;
 
     SparseGroupSlope penalty(lambda, kappa);
-    std::cout << "a" << std::endl;;
 
     const double init[] = {15, 10, 5, 12, 8, 4, 9, 6, 3, 6, 4, 2, 3, 2, 1};
     arma::mat B(&init[0], 3, 5);
-    //arma::mat B(3, 5, arma::fill::randu);
-    std::cout << "B:" << std::endl;;
-    std::cout << B << std::endl;
 
-    std::cout << "lambda:";
-    std::cout << lambda << std::endl;
 
-    std::cout << "kappa:";
-    std::cout << kappa << std::endl;
-
-    std::cout << "evaluate:" << penalty.evaluate(B) << std::endl;
-
-    std::cout << "prox:";
-    std::cout << penalty.prox(B) << std::endl;
-    expect_true(4 == 4);
+    expect_true(std::abs(penalty.evaluate(B) - 433.826778819339319) < 1e-10);
+    expect_true(arma::norm(penalty.prox(B) - expected_prox, "inf") < 1e-10);
   }
 
 }
